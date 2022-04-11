d7cf2d0fd12b9537e86e91e4ca76c4d186d1afaf
Parallax18 <rhythmist204@gmail.com>
2022-04-11 02:20:53 +0100

    fix: dashboard sidebar accordion and state

--- components/DrawerItem.js
+++ components/DrawerItem.js
@@ -1,14 +1,61 @@
-import { Button, Icon,  Text, Image } from '@chakra-ui/react'
-import React from 'react'
-import { IoRocketOutline } from "react-icons/io5"
-// import { rocket } from "../assets/icons"
+import {
+  Button, Icon,  Text, Image,
+  Accordion,
+  AccordionItem,
+  AccordionButton,
+  AccordionPanel,
+  AccordionIcon,
+  Box
+} from '@chakra-ui/react'
+import React, { Fragment, useState } from 'react'
 
-export default function DrawerItem({icon, buttonText}) {
+export default function DrawerItem({icon, buttonText, options}) {
+  
+  const [accordionIsOpen, setAccordionIsOpen] = useState(false)
+  const toggleAccordion = () => {
+    console.log('clicked', accordionIsOpen)
+    setAccordionIsOpen(!accordionIsOpen)
+  }
   return (
-    <Button w="full" background="none" outline={"none"}>
-      <Icon as={icon} marginRight={7} color="#c4c4c4" w={6} h={6}/>
-    
-      <Text color="#BFBFBF" w='full' textAlign={'left'} fontSize={14}>{buttonText}</Text>
-    </Button>
+   
+    <Fragment>
+      {options ? (
+         <Accordion allowToggle w="full">
+         <AccordionItem w="full"  border="none" onClick={toggleAccordion}>
+             <AccordionButton className='space-x-5' w="full" background="none" outline={"none"} _focus={{outline: 'none'}} _active={{outline: 'none'}}>
+               
+                <div className={accordionIsOpen && 'bg-indigo-900 rounded-full h-12 w-12 p-3 flex items-center justify-center'} >
+                 <Icon as={icon}  color={accordionIsOpen ? "#ffff" : "#c4c4c4"} w={6} h={6}/>
+                </div>
+               
+                 <Text color={accordionIsOpen ? '#333' : "#BFBFBF"} w='full' textAlign={'left'} fontSize={14}>{buttonText}</Text>
+               
+               <AccordionIcon color="#BFBFBF"/>
+             </AccordionButton>
+           <AccordionPanel pb={4} display={'flex'} alignItems="flex-end" flexDirection="column">
+             {options.map((option) => (
+               <Button w="full" textAlign={'left'} background="none"  outline={"none"} _focus={{outline: 'none'}} _active={{outline: 'none'}} color="#cccc"  fontSize={14}>
+                 <Text className='w-2/3' textAlign={'left'}>{option}</Text>
+               </Button>
+             ))}
+           </AccordionPanel>
+         </AccordionItem>
+       </Accordion>
+      ) : 
+          <Button w="full" background="none" outline={"none"} color="#BFBFBF" _focus={{outline: 'none'}} _active={{outline: 'none'}} justifyContent={"left"} fontSize={14} >
+            <Icon as={icon} marginRight={7} color="#c4c4c4" w={6} h={6}/>
+            {buttonText}
+          
+        </Button>
+      }
+    </Fragment>
+   
   )
+}
+
+
+const styles = {
+  menulist: {
+      background:  'none'
+  }
 }
--- components/SideDrawer.js
+++ components/SideDrawer.js
@@ -4,47 +4,62 @@ import DrawerItem from './DrawerItem'
 import { IoRocketOutline } from "react-icons/io5";
 import { MdOutlineDashboard } from "react-icons/md"
 import { RiWallet3Line } from "react-icons/ri"
-import { BsCart2, BsBank } from "react-icons/bs";
+import { BsCart2, BsBank, BsClipboardData } from "react-icons/bs";
 import { FiSettings, FiUsers } from "react-icons/fi";
 import { BiLogOut } from "react-icons/bi";
+import { GiAbacus } from "react-icons/gi"
 
 
 
 const data = [
   {
-    icon: MdOutlineDashboard ,
-    buttonText: 'Dashboard'
-  },
-  {
     icon: IoRocketOutline,
     buttonText: 'Welcome'
   },
   {
+    icon: MdOutlineDashboard ,
+    buttonText: 'Dashboard'
+  },
+  {
     icon: RiWallet3Line,
-    buttonText: 'Sales'
+    buttonText: 'Sales',
+    options: ['bills', 'vendors', 'products and services']
   },
   {
     icon: BsCart2,
-    buttonText: 'Purchases'
+    buttonText: 'Purchases',
+    options: ['bills', 'vendors', 'products and services']
+  },
+  {
+    icon: GiAbacus,
+    buttonText: 'Accounting'
   },
   {
     icon: BsBank,
-    buttonText: 'Banking'
+    buttonText: 'Banking',
+    options: ['bills', 'vendors', 'products and services']
+  },
+  {
+    icon: BsClipboardData,
+    buttonText: 'Report'
   },
   {
     icon: FiUsers,
-    buttonText: 'Analytics'
+    buttonText: 'Analytics',
+    options: ['bills', 'vendors', 'products and services']
   },
   {
     icon: FiSettings,
     buttonText: 'Settings'
-  }
+  },
 ]
 
 
 export default function SideDrawer() {
   const { isOpen, onOpen, onClose } = useDisclosure()
   const [dashboardItem, setDashboarditem] = useState(data)
+  // const [accordionIsOpen, setAccordionIsOpen] = useState(false)
+ 
 
   return (
     <Drawer
@@ -52,28 +67,23 @@ export default function SideDrawer() {
         placement='left'
         onClose={onClose}
         size={'xs'}
-        // finalFocusRef={btnRef}
       >
     
         <DrawerContent className='bg-red-900' sx={{background: '#F9F9F9'}}>
-          <DrawerCloseButton />
-          <DrawerHeader>Create your account</DrawerHeader>
+          {/* <DrawerCloseButton /> */}
+          <DrawerHeader>Hello, Sam</DrawerHeader>
 
           <DrawerBody>
-            <VStack spacing={4}>
-              {dashboardItem.map((item) => <DrawerItem icon={item.icon} buttonText={item.buttonText}/>)}
-              {/* <DrawerItem />
-              <DrawerItem />
-              <DrawerItem />
-              <DrawerItem /> */}
+            <VStack spacing={4} >
+              {dashboardItem.map((item) => <DrawerItem icon={item.icon} buttonText={item.buttonText} options={item.options} />)}
             </VStack>
           </DrawerBody>
 
           <DrawerFooter>
-            <Button variant='outline' mr={3} onClick={onClose}>
+            {/* <Button variant='outline' mr={3} onClick={onClose}>
               Cancel
             </Button>
-            <Button colorScheme='blue'>Save</Button>
+            <Button colorScheme='blue'>Save</Button> */}
           </DrawerFooter>
         </DrawerContent>
       </Drawer>
